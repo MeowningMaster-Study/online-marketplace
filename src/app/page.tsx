2db8db054d1df5093f01bb18312663d6b3c74980
MeowningMaster <meowningmaster@gmail.com>
2023-10-19 07:47:05 +0300

    drizzle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import Image from 'next/image'
 import {db} from '@/lib/drizzle'
+import { test } from '@/lib/drizzle/schema/test'
 
-export default function Home() {
+export default async function Home() {
+  const records = await db.select().from(test)
+  console.log(records)
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
