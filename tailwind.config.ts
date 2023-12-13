86b0529ec154d6c77322d069df3e110363d3c163
Andrii Semenystyi <46621668+MeowningMaster@users.noreply.github.com>
2023-12-13 17:47:18 +0200

    fix style conflict

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,11 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './src/**/*.{js,ts,jsx,tsx,mdx}'
-  ],
-  theme: {},
-  plugins: [],
+	content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+	theme: {},
+	plugins: [],
+	corePlugins: {
+		preflight: false,
+	},
 }
 export default config
