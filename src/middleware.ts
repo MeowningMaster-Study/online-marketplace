7e1c674203c62842155ed31538315830a8c16ae2
Andrii Semenystyi <46621668+MeowningMaster@users.noreply.github.com>
2023-12-13 18:26:43 +0200

    security

---
+++ src/middleware.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import { auth } from '@/utilities/auth'
+
+export async function middleware(request: NextRequest) {
+	if (!auth.get()) {
+		return NextResponse.redirect(new URL('/login', request.url))
+	}
+}
+
+export const config = {
+	matcher: ['/((?!api|_next/static|_next/image|favicon.ico|login|sign-up).*)'],
+}
